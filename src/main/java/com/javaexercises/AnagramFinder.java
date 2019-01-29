067abbd7e75e5b351d6b00301caef8843e04704c
mgama <mgamamsc@gmail.com>
2019-01-29 10:10:11 -0500

    Adding more logic to the isAnagram function and adding inputDataIsValid function to the AnagramFinder class

--- src/main/java/com/javaexercises/AnagramFinder.java
+++ src/main/java/com/javaexercises/AnagramFinder.java
@@ -2,15 +2,39 @@ package JavaExercises;
 
 public class AnagramFinder {
 	
-	public String firstWord;
-	public String secondWord; 
-
-	public AnagramFinder(String stringOne, String stringTwo) {
-		this.firstWord = stringOne;
-		this.secondWord = stringTwo;
+	public boolean inputDataIsValid(Object inputData) {
+		boolean dataIsValid = false;
+		try{
+			if (inputData instanceof Integer) {
+				dataIsValid = false;
+				System.out.println("Detected inputData to be an Integer");
+			}
+			else if (inputData instanceof String) {
+				dataIsValid = true;
+				System.out.println("Detected inputData to be a String");
+			}
+		}
+		catch(Exception e){
+			System.out.println("The inputData " + inputData + "was not a string");
+		}
+		return dataIsValid;
 	}
 
-	public boolean isAnagram(){
-		return false;
+	public boolean isAnagram(String stringOne, String stringTwo){
+		//Verify that the firstWord and secondWord variables 
+		//are strings. Otherwise, return false
+		if (inputDataIsValid(stringOne) && inputDataIsValid(stringTwo)){
+			//Review the lengths of both strings. If they are different, 
+			//then immediately return false
+			if (stringOne.length() == stringTwo.length()) {
+				return true;
+			}
+			else {
+				return false; 
+			}
+		}
+		else {
+			return false;
+		}
 	}
 }
--- src/test/java/com/javaexercises/TestAnagram.java
+++ src/test/java/com/javaexercises/TestAnagram.java
@@ -6,13 +6,18 @@ import JavaExercises.AnagramFinder;
 @Test
 public class TestAnagram {
   @Test(groups = { "anagram" })
+  public void checkAnagramIsFalseBecauseOfInvalidInput() {
+	AnagramFinder anagramFinder = new AnagramFinder();
+	Assert.assertFalse(anagramFinder.isAnagram("","help"), "Asserting the anagram is false since it had invalid input data (empty string)");
+  }
+
   public void checkAnagramIsFalse() {
-	AnagramFinder anagramFinder = new AnagramFinder("hello","help");
-	Assert.assertFalse(anagramFinder.isAnagram(), "Asserting the anagram is false for hello and help");
+	AnagramFinder anagramFinder = new AnagramFinder();
+	Assert.assertFalse(anagramFinder.isAnagram("hello","help"), "Asserting the anagram is false for hello and help");
   }
 
   public void checkAnagramIsTrue() {
-	AnagramFinder anagramFinder = new AnagramFinder("post","stop");
-	Assert.assertTrue(anagramFinder.isAnagram(), "Asserting the anagram is true for post and stop");
+	AnagramFinder anagramFinder = new AnagramFinder();
+	Assert.assertTrue(anagramFinder.isAnagram("post","stop"), "Asserting the anagram is true for post and stop");
   }
 }
