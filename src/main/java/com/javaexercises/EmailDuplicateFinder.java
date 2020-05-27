8d9939d88042a11bab9b336b6f26535590d3f161
Manuel Gama <mgamamsc@gmail.com>
2020-05-26 21:05:22 -0400

    Adding EmailDuplicateFinder class

---
+++ src/main/java/com/javaexercises/EmailDuplicateFinder.java
@@ -0,0 +1,35 @@
+// package JavaExercises;
+
+import java.util.*; 
+
+public class EmailDuplicateFinder {
+
+	public static void main(String[] args) 
+    { 
+  
+        // create a hash set
+      	HashSet hs = new HashSet();
+  
+        hs.add(1, "mgama12345@gmail.com"); 
+        hs.add(2, "sford3456@hotmail.com"); 
+        hs.add(3, "hwolfram5677@hotmail.com"); 
+        hs.add(4, "greyes789@yahoo.com"); 
+        hs.add(5, "j.tesla789@gmail.com");
+        hs.add(6, "j.tesla789@gmail.com");
+  
+        // // Displaying the HashMap 
+        // System.out.println("Initial Mappings are: " + hash_map); 
+  
+        // // Checking for the Value 'j.tesla789@gmail.com' 
+        System.out.println("Is the value 'j.tesla789@gmail.com' present? " +  
+        hash_map.containsValue("j.tesla789@gmail.com"));
+
+        int counter = 0; 
+        filtered_hash_map.put(counter, ); 
+  
+        // // Checking for the Value 'World' 
+        // System.out.println("Is the value 'World' present? " +  
+        // hash_map.containsValue("World")); 
+    } 
+
+}
