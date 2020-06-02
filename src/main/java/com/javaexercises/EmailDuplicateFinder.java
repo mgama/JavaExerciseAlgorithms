c8f905f29129fa745fb975c5f7ca96369d29159c
Manuel Gama <mgama@wayfair.com>
2020-06-01 20:33:53 -0400

    Adding Mock Data csv with email addresses and final implementation of hashset

--- src/main/java/com/javaexercises/EmailDuplicateFinder.java
+++ src/main/java/com/javaexercises/EmailDuplicateFinder.java
@@ -1,35 +1,44 @@
 // package JavaExercises;
 
-import java.util.*; 
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class EmailDuplicateFinder {
 
-	public static void main(String[] args) 
-    { 
-  
+	public static void main(String[] args)
+    {
+
         // create a hash set
       	HashSet hs = new HashSet();
-  
-        hs.add(1, "mgama12345@gmail.com"); 
-        hs.add(2, "sford3456@hotmail.com"); 
-        hs.add(3, "hwolfram5677@hotmail.com"); 
-        hs.add(4, "greyes789@yahoo.com"); 
-        hs.add(5, "j.tesla789@gmail.com");
-        hs.add(6, "j.tesla789@gmail.com");
-  
-        // // Displaying the HashMap 
-        // System.out.println("Initial Mappings are: " + hash_map); 
-  
-        // // Checking for the Value 'j.tesla789@gmail.com' 
-        System.out.println("Is the value 'j.tesla789@gmail.com' present? " +  
-        hash_map.containsValue("j.tesla789@gmail.com"));
 
-        int counter = 0; 
-        filtered_hash_map.put(counter, ); 
-  
-        // // Checking for the Value 'World' 
-        // System.out.println("Is the value 'World' present? " +  
-        // hash_map.containsValue("World")); 
-    } 
+        //Sample set
+        // hs.add("mgama12345@gmail.com");
+        // hs.add("sford3456@hotmail.com");
+        // hs.add("hwolfram5677@hotmail.com");
+        // hs.add("greyes789@yahoo.com");
+        // hs.add("j.tesla789@gmail.com");
+        // hs.add("j.tesla789@gmail.com");
+
+        String csvFile = "MOCK_DATA.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) { //Read the CSV with the email addresses
+          while ((line = br.readLine()) != null) {
+              String[] email = line.split(cvsSplitBy); //Read each email address one by one
+              hs.add(email[0]); //Add each email address to the hash set. If the value is duplicated, it wont be added.
+          }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(hs);
+        System.out.println(hs.size());
+        //With the MOCK_DATA.csv test file, they are 500 duplicated email addresses out of 1500, therefore the hashset will be
+        //only 1000 in size
+    }
 
 }
